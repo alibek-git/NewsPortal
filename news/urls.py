593390ba5225b8ea78700b0e4bba132940ac1912
Alibek <alibek1991@gmail.com>
2021-09-03 12:18:50 +0600

    D4 Module assignments applied: Filters and Forms added; Added urls for deleting and editing pages

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<int:pk>', PostDetailedView.as_view(), name='post'),
     path('search/', PostSearchView.as_view(), name='post_search'),
     path('create/', PostCreateView.as_view(), name='post_create'),
-    path('create/', PostUpdateView.as_view(), name='post_create'),
-    path('delete', PostDeleteView.as_view(), name='post_delete'),
+    path('<int:pk>/create/', PostUpdateView.as_view(), name='post_create'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
