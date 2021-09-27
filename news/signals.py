d40eb0dd081ad2c0b3179e967ddaed0d017d942b
Alibek <alibek1991@gmail.com>
2021-09-27 12:57:33 +0600

    D6 Module: Assignment attempted

--- news/signals.py
+++ news/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Author
+from django.core.mail import mail_managers
 
 
 @receiver(post_save, sender=Author)
