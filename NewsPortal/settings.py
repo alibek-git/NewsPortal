d8d062a07ab5e0fc7ff4b063a579e3bef3cf1dcb
Alibek <83647897+alibek-git@users.noreply.github.com>
2023-05-19 22:04:24 +0600

    Add STATIC_ROOT and edit STATICFILES_DIRS variables

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -170,9 +170,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'NewsPortal/static/'
+STATIC_ROOT = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / "NewsPortal/static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
